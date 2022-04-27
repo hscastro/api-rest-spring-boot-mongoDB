6e51e5263be7b063eef7d39cdeb11c39408e69b4
hscastro <halisonsc5@gmail.com>
2022-04-27 13:42:12 -0300

    Modified in Book class

--- spring-boot-mongodb/src/main/java/com/hscastro/example/entities/Book.java
+++ spring-boot-mongodb/src/main/java/com/hscastro/example/entities/Book.java
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
-@Document
+@Document(collection = "book")
 public class Book {
 
 	@Id
